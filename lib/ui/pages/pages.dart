e0d420a72146700b0f0aca519dfb5021987678de
fathlaziss <71271714+fathlaziss@users.noreply.github.com>
2021-01-14 10:32:49 +0700

    adding general page

--- lib/ui/pages/pages.dart
+++ lib/ui/pages/pages.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:smartlink_test/ui/widgets/widgets.dart';
 import 'package:supercharged/supercharged.dart';
 import 'package:smartlink_test/shared/shared.dart';
 
 part 'main_page.dart';
 part 'splash_page.dart';
+part 'general_page.dart';
